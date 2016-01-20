8679e73585a1b7cb1b2ec047a997d67ee6679cfd
Revathy Manulal <revathymanulal@yahoo.com>
2016-01-20 12:35:57 +0530

    Added JustJava

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int noOfCoffees=2;
+    public void submitOrder(View view) {
+        display(noOfCoffees);
+        displayPrice(noOfCoffees*5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view){
+        noOfCoffees=noOfCoffees+1;
+        display(noOfCoffees);
+    }
+
+    public void decrement(View view){
+        if(noOfCoffees>0){
+        noOfCoffees=noOfCoffees-1;}
+        display(noOfCoffees);
+    }
+}
