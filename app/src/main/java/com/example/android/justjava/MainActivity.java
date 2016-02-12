8c60be81b36f436b65e602466cfb05c2aecb21de
Revathy Manulal <revathymanulal@yahoo.com>
2016-02-13 01:00:38 +0530

    Added EditText and checkBox

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -20,15 +22,36 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     int noOfCoffees=2;
-
+    Boolean ch;
+    Boolean co;
+    String customerName;
     public void submitOrder(View view) {
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
+        if (checkBox.isChecked()) {
+           ch=true;
+        }
+        else
+        {
+            ch=false;
+        }
 
+        CheckBox checkBox1=(CheckBox) findViewById(R.id.checkbox1);
+        if (checkBox1.isChecked()){
+            co=true;
+        }
+        else {
+            co=false;
+        }
+
+        EditText editText=(EditText) findViewById(R.id.name);
+        customerName=editText.getText().toString();
         displayMessage(createOrderSummary(noOfCoffees));
+
 
     }
     private String createOrderSummary(int noOfCoffees){
 
-        String priceMessage="Name: Kaptain Kunal\nQuantity: "+noOfCoffees+"\nTotal:$"+noOfCoffees*5+"\nThank you!";
+        String priceMessage="Name: "+customerName+"\nAdd whipped cream? "+ch+"\nAdd chocolate? "+co+"\nQuantity: "+noOfCoffees+"\nTotal:$"+noOfCoffees*5+"\nThank you!";
         return priceMessage;
     };
 
@@ -57,8 +80,8 @@ public class MainActivity extends ActionBarActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     public void increment(View view){
